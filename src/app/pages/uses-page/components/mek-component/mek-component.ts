39fd166bdf55b2877c6f1603d6c5f900f69f03a3
tanespol916 <mek.tanespol@gmail.com>
2026-01-13 09:52:37 +0700

    create mek-component

---
+++ src/app/pages/uses-page/components/mek-component/mek-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mek-component',
+  imports: [],
+  templateUrl: './mek-component.html',
+  styleUrl: './mek-component.css',
+})
+export class MekComponent {
+
+}
