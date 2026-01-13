6bc7b8977ae3c7bc1186d4a0ed4ba82ef7cbbb08
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2026-01-13 09:54:15 +0700

    add shogun-component

---
+++ src/app/pages/uses-page/components/shogun-component/shogun-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shogun-component',
+  imports: [],
+  templateUrl: './shogun-component.html',
+  styleUrl: './shogun-component.css',
+})
+export class ShogunComponent {
+
+}
