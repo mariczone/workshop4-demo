ed50649103e85935e1398071582923f705c3d230
Kotchapan Lertpanyaroj <kotchapan2992@gmail.com>
2026-01-13 10:00:19 +0700

    Create numfha-component

---
+++ src/app/pages/uses-page/components/numfha-component/numfha-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numfha-component',
+  imports: [],
+  templateUrl: './numfha-component.html',
+  styleUrl: './numfha-component.css',
+})
+export class NumfhaComponent {
+
+}
