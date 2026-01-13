ce086574d7e38981b9473ec5e01e98b18c8e7794
thanathatp <alapikacoffdev@gmail.com>
2026-01-13 09:57:00 +0700

    add march-component in uses-page

---
+++ src/app/pages/uses-page/components/march-component/march-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-march-component',
+  imports: [],
+  templateUrl: './march-component.html',
+  styleUrl: './march-component.css',
+})
+export class MarchComponent {
+
+}
