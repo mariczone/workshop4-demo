b94c2ab230078b84daf9f36e870bf35ef3f5c0dd
Apivit Y. <120260881+IceEz555@users.noreply.github.com>
2026-01-13 09:56:47 +0700

    complete component

---
+++ src/app/pages/uses-page/components/ice-component/ice-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ice-component',
+  imports: [],
+  templateUrl: './ice-component.html',
+  styleUrl: './ice-component.css',
+})
+export class IceComponent {
+
+}
